e0683add534c2bc86d13051b0ae8c20432901b88
Apine <apineorbeenga@gmail.com>
2026-04-24 10:12:34 +0100

    Polish inquiry status interactions

--- src/features/inquiries/hooks/index.ts
+++ src/features/inquiries/hooks/index.ts
@@ -3,6 +3,7 @@ export { useSubmitInquiry } from "./useSubmitInquiry";
 export { useInquiryFeed } from "./useInquiryFeed";
 export {
   useInquiryDirectory,
+  useMarkInquiryViewed,
   useUpdateInquiryStatus,
   type InquiryDirectorySource,
 } from "./useInquiryDirectory";
