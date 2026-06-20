1e6b57bfe6547d34272409d7f06372d9799aa1e1
Apine <apineorbeenga@gmail.com>
2026-06-20 12:23:07 +0100

    P.2: separate agency dashboard from membership management

---
+++ src/app/(main)/account/agency/members/page.tsx
@@ -0,0 +1,7 @@
+import { AgencyMembersClient } from "@/features/agencies/components/AgencyMembersClient";
+
+export const dynamic = "force-dynamic";
+
+export default function AgencyMembersPage() {
+  return <AgencyMembersClient />;
+}
